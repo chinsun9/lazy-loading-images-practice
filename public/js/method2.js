c3a8c7b9c366c0930941e36b2566e3cb4f797ce0
chinsun9 <chin_sung@naver.com>
2021-07-16 14:27:33 +0900

    feat: add root margin

--- public/js/method2.js
+++ public/js/method2.js
@@ -3,19 +3,22 @@ export default () => {
 
   if ("IntersectionObserver" in window) {
     lazyloadImages = document.querySelectorAll(".lazy");
-    const imageObserver = new IntersectionObserver(function (
-      entries,
-      observer
-    ) {
-      entries.forEach(function (entry) {
-        if (entry.isIntersecting) {
-          const image = entry.target;
-          image.src = image.dataset.src;
-          image.classList.remove("lazy");
-          imageObserver.unobserve(image);
-        }
-      });
-    });
+    const imageObserver = new IntersectionObserver(
+      function (entries, observer) {
+        entries.forEach(function (entry) {
+          if (entry.isIntersecting) {
+            const image = entry.target;
+            image.src = image.dataset.src;
+            image.classList.remove("lazy");
+            imageObserver.unobserve(image);
+          }
+        });
+      },
+      {
+        root: document.querySelector(".container"),
+        rootMargin: "0px 0px 500px 0px",
+      }
+    );
 
     lazyloadImages.forEach(function (image) {
       imageObserver.observe(image);
