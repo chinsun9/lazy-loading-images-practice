43a9aa1f69cedb965721693e81002a5acb873660
chinsun9 <chin_sung@naver.com>
2021-07-16 14:09:02 +0900

    feat: backgroundimage lazy loading

---
+++ public/js/background-image.js
@@ -0,0 +1,18 @@
+var lazyloadImages;
+
+if ("IntersectionObserver" in window) {
+  lazyloadImages = document.querySelectorAll(".lazy");
+  var imageObserver = new IntersectionObserver(function (entries, observer) {
+    entries.forEach(function (entry) {
+      if (entry.isIntersecting) {
+        var image = entry.target;
+        image.classList.remove("lazy");
+        imageObserver.unobserve(image);
+      }
+    });
+  });
+
+  lazyloadImages.forEach(function (image) {
+    imageObserver.observe(image);
+  });
+}
